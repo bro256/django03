453a0b195f5c8f7d567a83f841d4410eae798c28
bro256 <bronius256@gmail.com>
2023-06-15 22:50:44 +0300

    models

--- django03/task_manager/models.py
+++ django03/task_manager/models.py
@@ -1,3 +1,138 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
-# Create your models here.
+User = get_user_model()
+
+# class Priority(models.Model):
+#     content = models.CharField(_('content'), max_length=100, null=True, blank=True)
+
+#     class Meta:
+#         verbose_name = _("priority")
+#         verbose_name_plural = _("priorities")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("priority_detail", kwargs={"pk": self.pk})
+    
+
+# class Status(models.Model):
+#     content = models.CharField(_('content'), max_length=50)
+
+#     class Meta:
+#         verbose_name = _("status")
+#         verbose_name_plural = _("statuses")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("status_detail", kwargs={"pk": self.pk})
+
+
+class Task(models.Model):
+    content = models.CharField(_('content'), max_length=250)
+    start = models.DateField
+    finish = models.DateField
+    owner = models.ForeignKey(
+        User,
+        verbose_name=_("owner"),
+        on_delete=models.CASCADE,
+        related_name='owner_tasks',
+        null=True, blank=True,
+    )
+    assignee = models.ForeignKey(
+        User,
+        verbose_name=_("assignee"),
+        on_delete=models.CASCADE,
+        related_name='assignee_tasks',
+        null=True, blank=True,
+    )
+    # priority = models.ForeignKey(
+    #     Priority,
+    #     verbose_name=_("priority"),
+    #     on_delete=models.CASCADE,
+    #     related_name='tasks',
+    #     null=True, blank=True,
+    # )
+    # status = models.ForeignKey(
+    #     Status,
+    #     verbose_name=_('status'),
+    #     on_delete=models.CASCADE,
+    #     related_name='tasks',
+    #     null=True, blank=True,
+    # )
+
+    PRIORITY_CHOICES = (
+        (1, _('Low')),
+        (2, _('Medium')),
+        (3, _('High')),
+        (4, _('Critical')),
+    )
+    priority = models.PositiveSmallIntegerField(
+        _("priority"), 
+        choices=PRIORITY_CHOICES, 
+        default=0,
+        db_index=True,
+        null=True, blank=True,
+    )
+
+    STATUS_CHOICES = (
+        (0, _('Not started')),
+        (1, _('In progess')),
+        (2, _('Completed')),
+        (3, _('On hold')),
+        (4, _('Cancelled')),
+    )
+    status = models.PositiveSmallIntegerField(
+        _("status"), 
+        choices=STATUS_CHOICES, 
+        default=0,
+        db_index=True,
+        null=True, blank=True,
+    )
+    created_at = models.DateTimeField(_("Created"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("task")
+        verbose_name_plural = _("tasks")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("task_detail", kwargs={"pk": self.pk})
+    
+
+class TaskComment(models.Model):
+    content = models.CharField(_('content'), max_length=250)
+    task = models.ForeignKey(
+        Task,
+        verbose_name=_("task"),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        null=True, blank=True,
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("user"),
+        on_delete=models.CASCADE,
+        related_name='task_comments',
+        null=True, blank=True,
+    )
+    created_at = models.DateTimeField(_("Created"), auto_now_add=True)
+    
+    class Meta:
+        verbose_name = _("task comment")
+        verbose_name_plural = _("task comments")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("taskcomment_detail", kwargs={"pk": self.pk})
+
+
