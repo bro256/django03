39162bc2eb74d55f9e8b20b333c300a53b7f53e1
bro256 <bronius256@gmail.com>
2023-06-16 13:13:04 +0300

    views and base.html

--- django03/task_manager/views.py
+++ django03/task_manager/views.py
@@ -1,4 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Task, TaskComment
 
 def index(request):
-    return render(request,'index.html')
+    num_tasks = Task.objects.all().count()
+
+    # To dictionary
+    context = {
+        'num_tasks' : num_tasks,
+    }
+
+    return render(request, 'task_manager/index.html', context)
