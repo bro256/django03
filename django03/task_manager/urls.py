24a6bd32ae45fbdf20d00e8aafd15c5c4a1cc44d
bro256 <bronius256@gmail.com>
2023-06-16 15:46:03 +0300

    task_list, view, paginator

--- django03/task_manager/urls.py
+++ django03/task_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tasks/', views.TaskListView.as_view(), name='task_list'),
 ]
--- django03/task_manager/views.py
+++ django03/task_manager/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . models import Task, TaskComment
+from django.views import generic
+
 
 def index(request):
     num_tasks = Task.objects.all().count()
@@ -11,3 +13,11 @@ def index(request):
     }
 
     return render(request, 'task_manager/index.html', context)
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 2
+    template_name = "task_manager/task_list.html"
+
+    
+
