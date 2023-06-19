1fd19587f96550eb571b38131d42cdcabc4d6c1b
bro256 <bronius256@gmail.com>
2023-06-19 19:41:52 +0300

    date input added

--- django03/task_manager/forms.py
+++ django03/task_manager/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from . import models
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = models.Task
         fields = ('content', 'start', 'finish', 'owner', 'assignee', 'priority', 'status')
         widgets = {
             'owner' : forms.HiddenInput(),
+            'start' : DateInput(),
+            'finish' : DateInput(),
         }
