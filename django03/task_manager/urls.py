6ad1eeeca665aefadb629a002ee63153eed6be49
bro256 <bronius256@gmail.com>
2023-06-19 16:47:48 +0300

    New task functionality added

--- django03/task_manager/urls.py
+++ django03/task_manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('tasks/', views.TaskListView.as_view(), name='task_list'),
     path('task/<int:pk>/', views.TaskDetailView.as_view(), name='task_detail'),
     path('task/update/<int:pk>/', views.TaskUpdateView.as_view(), name='task_update'),
+    path('task/create/', views.TaskCreateView.as_view(), name='task_create'),
 ]
--- django03/task_manager/views.py
+++ django03/task_manager/views.py
@@ -1,8 +1,11 @@
+from datetime import date, timedelta
+from typing import Any, Dict, Optional
 from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from . forms import TaskForm
 from . models import Task, TaskComment
@@ -29,6 +32,35 @@ class TaskDetailView(generic.DetailView):
     model = Task
     template_name = "task_manager/task_detail.html"
 
+
+class TaskCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_manager/task_form.html'
+    # success_url = reverse_lazy('task_list')
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def get_initial(self) -> Dict[str, Any]:
+        # self.task = get_object_or_404(Task, id=self.request.GET.get('task_id'))
+        initial = super().get_initial()
+        initial['start'] = date.today()
+        initial['finish'] = date.today() + timedelta(days=14)
+        initial['status'] = 0
+        initial['priority'] = 2
+        initial['owner'] = self.request.user
+        return initial
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+    # Checks that the user passes the given test
+    def test_func(self):
+        return self.request.user.is_authenticated
+                
 
 class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Task
