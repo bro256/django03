44181fc10efb510059fecf30425152edfe189d42
bro256 <bronius256@gmail.com>
2023-06-19 09:12:13 +0300

    User login

--- django03/django03/settings.py
+++ django03/django03/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'task_manager',
+    'user_profile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
--- django03/django03/urls.py
+++ django03/django03/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('task_manager.urls')),
+    path('profile/', include('user_profile.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
