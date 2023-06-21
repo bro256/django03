2049cd366b435528ae022fa342ff9ea6a0cbfeff
bro256 <bronius256@gmail.com>
2023-06-21 09:24:20 +0300

    messages in base.html

--- django03/user_profile/views.py
+++ django03/user_profile/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_protect
 
-# Create your views here.
+# @csrf_protect
+def signup(request):
+    if request.user.is_authenticated:
+        messages.info(request, 'In order to sign up, you need to logout first')
+        return redirect('index')
+    return render(request, 'user_profile/signup.html')
