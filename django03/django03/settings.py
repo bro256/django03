7051f14c228397a50a220af6b25798e9eeb2e1fb
bro256 <bronius256@gmail.com>
2023-06-23 08:26:49 +0300

    local settings and minor styling in comments

--- django03/django03/settings.py
+++ django03/django03/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = local_settings.ALLOWED_HOSTS
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = local_settings.STATIC_ROOT
 
 STATIC_URL = 'static/'
 
